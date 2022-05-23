14f5ccf5ab8244c4181555c16d75ba2f0a5dba1e
YukiHara1126 <36534387+YukiHara1126@users.noreply.github.com>
2022-05-24 08:28:01 +0900

    [Add] 基本のReact Router追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
-export default function App() {
+export function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+        {/* <Home />
+
+        <Page2 /> */}
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
